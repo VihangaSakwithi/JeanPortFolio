735fea96af37172ade5520089a17cc6796f1ad70
Vihanga Sakwithi <vsakwithi@outlook.com>
2023-07-11 12:46:44 +0530

    Add project files.

---
+++ Data/PortfolioDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using JeanPortFolio.Models;
+
+namespace JeanPortFolio.Data
+{
+    public class PortfolioDbContext : DbContext
+    {
+        public PortfolioDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Project> Projects { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+using JeanPortFolio.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+//Configure entity framework
+builder.Services.AddDbContext<PortfolioDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("JeanConnectionString")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
